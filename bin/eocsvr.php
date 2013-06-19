e5d120d192bf9ec7f73494e39e3f8e2174b0f1e0
Filippo Fadda <filippo.fadda@gmail.com>
2013-06-19 22:59:26 +0200

    from
    $loader = require_once __DIR__ . "/../vendor/autoload.php";
    
    to
    
    require_once __DIR__ . "/../vendor/autoload.php";

--- bin/eocsvr.php
+++ bin/eocsvr.php
@@ -6,7 +6,8 @@
 
 error_reporting (E_ALL & ~(E_NOTICE | E_STRICT));
 
-$loader = require_once __DIR__ . "/../vendor/autoload.php";
+// Initializes the Composer autoloading system.
+require_once __DIR__ . "/../vendor/autoload.php";
 
 // Creates and starts the server instance.
 $server = new Server();
